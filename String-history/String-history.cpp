9453dabc396de407964db460d1a363683c9b0939
chiheb <chihebabiza9@gmail.com>
2025-06-20 14:49:08 +0100

    string history done

--- String-history/String-history.cpp
+++ String-history/String-history.cpp
@@ -1,20 +1,55 @@
-// String-history.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "clsMyString.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    cout << "\n\n\t\t\t\t\t\t Undo/Redo Project\n\n";
+
+    clsMyString S1;
+
+    cout << "\nS1  = " << S1.Value << "\n";
+
+    S1.Value = "Mohammed";
+
+    cout << "S1  = " << S1.Value << "\n";
+
+    S1.Value = "Mohammed2";
+
+    cout << "S1  = " << S1.Value << "\n";
+
+    S1.Value = "Mohammed3";
+
+    cout << "S1  = " << S1.Value << "\n";
+
+    cout << "\n\nUndo: ";
+    cout << "\n__________\n";
+
+    S1.Undo();
+    cout << "\nS1  after undo = " << S1.Value << "\n";
+
+    S1.Undo();
+    cout << "S1  after undo = " << S1.Value << "\n";
+
+    S1.Undo();
+    cout << "S1  after undo = " << S1.Value << "\n";
+
+    cout << "\n\nRedo: ";
+    cout << "\n__________\n";
+
+    S1.Redo();
+    cout << "\nS1  after Redo = " << S1.Value << "\n";
+
+    S1.Redo();
+    cout << "S1  after Redo = " << S1.Value << "\n";
+
+    S1.Redo();
+    cout << "S1  after Redo = " << S1.Value << "\n";
+
+    system("pause>0");
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ String-history/clsMyString.cpp
@@ -0,0 +1,1 @@
+#include "clsMyString.h"
